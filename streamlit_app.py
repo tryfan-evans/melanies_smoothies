88ce3a7905123aa22b72e90cacda997c8b458bca
tryfan-evans <150818277+tryfan-evans@users.noreply.github.com>
2024-06-21 14:44:31 +0100

    Update streamlit_app.py
    adding a header

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,7 @@ if ingredients_list:
     
     for x in ingredients_list:
         ingredients_string += x+' '
+        st.subheader(x+' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+x)
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
