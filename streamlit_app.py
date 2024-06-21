a34b4e47d125cf6292266a6538050281cc61eb97
tryfan-evans <150818277+tryfan-evans@users.noreply.github.com>
2024-06-21 15:34:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,6 @@ if ingredients_list:
     
     for x in ingredients_list:
         ingredients_string += x+' '
-        searchon=
         st.subheader(x+' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+x)
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
